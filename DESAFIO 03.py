68bf781e1f576d0a5d16fd759db68e317ae261ec
Welvis Moreira <welvismoreira@gmail.com>
2017-11-09 00:24:47 -0200

    Add files via upload

---
+++ DESAFIO 03.py
@@ -0,0 +1,5 @@
+print ('==== DESAFIO 03 ====')
+numero1 = input ('Digite o primeiro numero: ')
+numero2 = input ('Digite o segundo numero: ')
+soma =  int(numero1)+ int(numero2) #FUNÇÃO PARA CONVERTER STRING EM INTEIRO.
+print('A soma de', numero1,'+',numero2, 'é', soma)
