34998eb7f30f3d5a6fc6f8f1db977531801e919a
Welvis Moreira <welvismoreira@gmail.com>
2017-11-09 22:54:43 -0200

    Add files via upload

---
+++ Aula07aD006.py
@@ -0,0 +1,2 @@
+numero = int(input('Digite um numero: '))
+print('O numero é {}, seu dobro é {}, seu Triplo é {} e sua raiz quadrada é {}'.format(numero, 2*numero, 3*numero, numero**(1/2) ))
---
+++ Aula07aD007.py
@@ -0,0 +1,3 @@
+nota = float(input('Digite a primeira nota: '))
+nota1 = float(input('Digite a segunda nota: '))
+print('A média do Aluno de nota {} e {} é {}'.format(nota, nota1, ((nota + nota1)/2)))
