aec4433d71f500e1ac15acc4d1e9166951085f03
Welvis Moreira <welvismoreira@gmail.com>
2017-11-26 20:36:56 -0200

    Implementando Comando IF
    Implementação do comando IF, foco na sintaxe apenas.

---
+++ Estrutura_IF.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um numero: '))
+if(numero >= 10 and numero <=20):
+    print("Numero maior que Dez!", numero)
+    print('entrou na faixa inicial')
+else:
+    print('Numero menor que 10 ou maior que 20, ', numero)
